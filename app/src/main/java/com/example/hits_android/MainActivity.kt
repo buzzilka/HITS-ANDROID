e8b6d55455cf59c993855c1511accdbd54dbb900
unknown <karpuk-05@mail.ru>
2024-05-09 01:06:33 +0700

    camera update

--- app/src/main/java/com/example/hits_android/MainActivity.kt
+++ app/src/main/java/com/example/hits_android/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.hits_android
 
-import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
@@ -21,6 +23,7 @@ import com.bumptech.glide.Glide
 import com.example.myapplication.viewmodel.MainViewModel
 import java.io.File
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +34,44 @@ class MainActivity : AppCompatActivity() {
         if (uri != null) {
             viewModel.selectFile(uri.toString())
         }
+    }
+
+
+    private val cameraPermissionRequestLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                startDefaultCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Go to settings and enable camera permission to use this feature",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private val takePictureLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Toast.makeText(this, "Photo taken", Toast.LENGTH_SHORT).show()
+        }
+
+    private fun handleCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                startDefaultCamera()
+            }
+
+            else -> {
+                cameraPermissionRequestLauncher.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun pickFilesFromGallery() {
+        filePickerLauncher.launch("image/*")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,13 +101,39 @@ class MainActivity : AppCompatActivity() {
 
         val cameraButton: ImageButton = findViewById(R.id.camera)
         cameraButton.setOnClickListener {
-
+            handleCameraPermission()
         }
     }
 
-    private fun pickFilesFromGallery() {
-        filePickerLauncher.launch("image/*")
+
+    private fun createImageFile(): File? {
+        val timeStamp: String = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            viewModel.selectFile(absolutePath)
+        }
     }
 
-
+    private fun startDefaultCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = createImageFile()
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    takePictureLauncher.launch(takePictureIntent)
+                } ?: run {
+                    Toast.makeText(this, "No camera app available", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 }
